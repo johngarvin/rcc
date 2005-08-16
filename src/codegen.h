b3ff72af0c483af15b6d2ac670d61e6c131360a7
John Mellor-Crummey <johnmc@cs.rice.edu>
2005-08-16 18:14:35 +0000

    added codegen.h and codegen.cc support for code generation.
    git-svn-id: file:///home/john/Code/repo-dumps/rcc/trunk@72 925864f1-7c04-44ca-bbaa-d8d9a55e96f5

---
+++ src/codegen.h
@@ -0,0 +1,40 @@
+#include <string>
+
+//------------------------------------------------------------------------------
+// emit conditional: if(expn) 
+//------------------------------------------------------------------------------
+std::string emit_logical_if(const std::string expn); 
+
+
+//------------------------------------------------------------------------------
+// emit conditional: if(expn) stmt;
+//------------------------------------------------------------------------------
+std::string emit_logical_if_stmt(const std::string expn, 
+				 const std::string stmt); 
+
+
+//------------------------------------------------------------------------------
+// emit assignment: lhs = rhs;
+//------------------------------------------------------------------------------
+std::string emit_assign(const std::string lhs, const std::string rhs); 
+
+
+//------------------------------------------------------------------------------
+// emit call: fname(...)
+//    note: sigh. arguments cannot be specified by varargs list for strings
+//------------------------------------------------------------------------------
+std::string emit_call0(const std::string fname);
+
+std::string emit_call1(const std::string fname, const std::string arg1);
+
+std::string emit_call2(std::string fname, const std::string arg1, 
+		       const std::string arg2);
+
+
+//------------------------------------------------------------------------------
+// emit call statement: fname(arg1, arg2, ...);
+//    note: arguments are specified as varargs list
+//          all will be interpreted as strings 
+//    note: statement terminated 
+//------------------------------------------------------------------------------
+std::string emit_call_stmt(const std::string fname, ...);
