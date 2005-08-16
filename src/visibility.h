1fdff5bd4cb711aab95c0c0c0d0f2a3c3406af3b
John Mellor-Crummey <johnmc@cs.rice.edu>
2005-08-16 18:11:14 +0000

    renamed parser support; added visibility support for printing.
    git-svn-id: file:///home/john/Code/repo-dumps/rcc/trunk@71 925864f1-7c04-44ca-bbaa-d8d9a55e96f5

---
+++ src/visibility.h
@@ -0,0 +1,12 @@
+#include <string>
+
+typedef enum {INVISIBLE, VISIBLE, CHECK_VISIBLE} visibility;
+
+class Visibility {
+public:
+  static std::string emit_set(visibility vis);
+  static std::string emit_set_if_visible(visibility vis);
+  static std::string emit_check_expn();
+private:
+  static const std::string R_VISIBILITY_GLOBAL_VAR;
+};
