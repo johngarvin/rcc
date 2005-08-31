926291014eff4f0ed4e373ad3882fa6f4240fadb
John Mellor-Crummey <johnmc@cs.rice.edu>
2005-08-31 05:34:47 +0000

    add boilerplate.
    git-svn-id: file:///home/john/Code/repo-dumps/rcc/trunk@117 925864f1-7c04-44ca-bbaa-d8d9a55e96f5

--- src/support/iterators/TraversalOrder.h
+++ src/support/iterators/TraversalOrder.h
@@ -1,7 +1,36 @@
+/* $Id: TraversalOrder.h,v 1.2 2005/08/31 05:34:47 johnmc Exp $ */
+//****************************************************************************/
+//                 Copyright (c) 1990-2005 Rice University
+//                          All Rights Reserved
+//****************************************************************************/
+
+//***************************************************************************
+// TraversalOrder.h
+//
+//   iterator traversal order specification
+//
+// Author: John Mellor-Crummey                                
+//
+// Creation Date: 30 August 2005
+//
+// Modification History:
+//  30 August 2005 -- John Mellor-Crummey
+//
+//**************************************************************************/
+
+  
 #ifndef TraversalOrder_h
 #define TraversalOrder_h
 
+
+//****************************************************************************
+// enumeration type declarations
+//***************************************************************************/
+
+
 typedef enum { Unordered, PreOrder, PostOrder,
            ReversePreOrder, ReversePostOrder, PreAndPostOrder } TraversalOrder;
+
+
 
 #endif
