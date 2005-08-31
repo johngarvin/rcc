eb5b3ea3eb1101fb0d4a94a15336773ace1ebacf
John Mellor-Crummey <johnmc@cs.rice.edu>
2005-08-31 23:34:22 +0000

    Split out the definition for property handles into a file separate from the property set itself.
    git-svn-id: file:///home/john/Code/repo-dumps/rcc/trunk@121 925864f1-7c04-44ca-bbaa-d8d9a55e96f5

---
+++ src/analysis/PropertyHndl.h
@@ -0,0 +1,32 @@
+// -*-Mode: C++;-*-
+// $Header: /home/garvin/cvs-svn/cvs-repos/developer/rcc/src/analysis/PropertyHndl.h,v 1.1 2005/08/31 23:34:22 johnmc Exp $
+
+// * BeginCopyright *********************************************************
+// *********************************************************** EndCopyright *
+
+//***************************************************************************
+//
+// File:
+//   $Source: /home/garvin/cvs-svn/cvs-repos/developer/rcc/src/analysis/PropertyHndl.h,v $
+//
+// Purpose:
+//   define a handle for properties
+//
+// Description:
+//   defines a single type PropertyHndlT. Instances of this handle type will
+//   be used to index into PropertySets.
+//
+//***************************************************************************
+
+
+#ifndef PropertyHndl.h
+#define PropertyHndl_h
+
+
+//***************************************************************************
+// Type definitions
+//***************************************************************************
+
+typedef const char* PropertyHndlT; // used to index into PropertySets.
+
+#endif
